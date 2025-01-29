455d3ee9f0a4e1e859b40709b183c058a967a455
unknown <amritdubey22003@gmail.com>
2025-01-29 07:21:38 +0530

    Minor Updates

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -23,8 +23,8 @@ export default function Form({getData}) {
             return;
         }
 
-        if(file.size>500*1024){
-            setErr("File size must be less than 500KB");
+        if(file.size>5*1000*1024){
+            setErr("File size must be less than 5MB");
             return;
         }
 
@@ -120,7 +120,7 @@ export default function Form({getData}) {
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none" viewBox="0 0 16 16">
                     <path stroke="#D1D0D5" stroke-linecap="round" stroke-linejoin="round" d="M2 8a6 6 0 1 0 12 0A6 6 0 0 0 2 8Z"/><path fill="#D1D0D5" d="M8.004 10.462V7.596ZM8 5.57v-.042Z"/><path stroke="#D1D0D5" stroke-linecap="round" stroke-linejoin="round" d="M8.004 10.462V7.596M8 5.569v-.042"/>
                 </svg>
-                <div>Upload your photo (JPG or PNG, max size: 500KB).</div>
+                <div>Upload your photo (JPG or PNG, max size: 5MB).</div>
             </span>}
 
             {err && 
